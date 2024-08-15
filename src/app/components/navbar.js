d992db56e2dc971d472a6d547b2c9f9556608ac0
Yossi-nordiansah <yossi.nordiansah99@gmail.com>
2024-08-15 09:02:57 +0700

    first commit

---
+++ src/app/components/navbar.js
@@ -0,0 +1,13 @@
+export default function Navbar() {
+    return (
+      <main className="py-5 px-6 bg-white">
+        <div className="flex ">
+            <div>
+                <select name="Country" id="">
+                    
+                </select>
+            </div>
+        </div>
+      </main>
+    );
+  }
